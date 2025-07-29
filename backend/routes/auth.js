9c400279e85ae643171b4a70291ee0d3be0d8629
may-ank-dot <albertnike1371@gmail.com>
2025-07-29 23:42:04 +0530

    backend setup

---
+++ backend/routes/auth.js
@@ -0,0 +1,11 @@
+import express from "express"
+import {z} from "zod"
+import { validateRequest } from "zod-express-middleware"
+import { registerSchema } from "../libs/validate-schema.js"
+import { registerUser } from "../controllers/auth-controller.js"
+
+const router = express.Router()
+
+router.post("/register",validateRequest({body: registerSchema}),registerUser) 
+
+export default router
